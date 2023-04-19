827fcd459019ba25373120187ad0ff2e4b49b81a
warriorincrystal123 <130286520+warriorincrystal123@users.noreply.github.com>
2023-04-19 16:06:04 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "take your meds") {
-        basic.showIcon(IconNames.Yes)
         basic.pause(100)
         servos.P0.setAngle(45)
         basic.pause(500)
         servos.P0.setAngle(135)
         basic.pause(500)
         servos.P0.stop()
+        basic.showIcon(IconNames.Yes)
     } else if (receivedString == "meds weren't taken") {
         basic.showIcon(IconNames.No)
     } else {
